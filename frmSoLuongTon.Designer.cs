35baf8b8dea75700ff216718c26f56bd5bc8d8e9
Winter-devg <donglatrum@gmail.com>
2024-12-08 11:17:34 +0700

    sửa lỗi không mở tồn kho
    nút số lượng tồn dưới thanh chức năng không thể mở
    gáng sự kiện nút tồn kho ở thanh công cụ cho số lượng tồn ở thanh chức năng

---
+++ frmSoLuongTon.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CuahangNongduoc
+{
+    partial class frmSoLuongTon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSoLuongTon));
+            this.SoLuongTonBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.SoLuongTonBindingSource)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SoLuongTonBindingSource
+            // 
+            this.SoLuongTonBindingSource.DataSource = typeof(CuahangNongduoc.BusinessObject.SoLuongTon);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.reportViewer);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1035, 575);
+            this.panel2.TabIndex = 1;
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "CuahangNongduoc_BusinessObject_SoLuongTon";
+            reportDataSource1.Value = this.SoLuongTonBindingSource;
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "CuahangNongduoc.Report.rptSoLuongTon.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.Size = new System.Drawing.Size(1035, 575);
+            this.reportViewer.TabIndex = 0;
+            // 
+            // frmSoLuongTon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 575);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmSoLuongTon";
+            this.Text = "SO LUONG TON";
+            this.Load += new System.EventHandler(this.frmSoLuongTon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SoLuongTonBindingSource)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+        private System.Windows.Forms.BindingSource SoLuongTonBindingSource;
+    }
+}
---
+++ frmSoLuongTon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CuahangNongduoc
+{
+    public partial class frmSoLuongTon : Form
+    {
+        public frmSoLuongTon()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSoLuongTon_Load(object sender, EventArgs e)
+        {
+            LoadSoLuongTon();
+        }
+
+        // Tách logic lấy dữ liệu ra thành phương thức riêng để cải thiện khả năng tái sử dụng và giảm độ phức tạp của sự kiện Load
+        private void LoadSoLuongTon()
+        {
+            try
+            {
+                var data = GetSoLuongTonData();
+                BindDataToReportViewer(data);
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage(ex);
+            }
+        }
+
+        // Thêm phương thức riêng để lấy dữ liệu
+        private IList<CuahangNongduoc.BusinessObject.SoLuongTon> GetSoLuongTonData()
+        {
+            return CuahangNongduoc.Controller.SanPhamController.LaySoLuongTon();
+        }
+
+        // Tách phần bind dữ liệu vào ReportViewer thành phương thức riêng
+        private void BindDataToReportViewer(IList<CuahangNongduoc.BusinessObject.SoLuongTon> data)
+        {
+            this.SoLuongTonBindingSource.DataSource = data;
+            this.reportViewer.RefreshReport();
+        }
+
+        // Thêm phương thức xử lý lỗi để tăng tính rõ ràng và tránh trùng lặp
+        private void ShowErrorMessage(Exception ex)
+        {
+            MessageBox.Show($"Đã xảy ra lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
